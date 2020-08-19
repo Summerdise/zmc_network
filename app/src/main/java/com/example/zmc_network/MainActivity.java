9c87f1fe27cca0a119ba8e27c37111a9ce5bc746
Summerdise <350464517@qq.com>
2020-08-19 15:28:00 +0800

    [zmc] feat: add same time geting http stutas showing function

--- app/src/main/java/com/example/zmc_network/MainActivity.java
+++ app/src/main/java/com/example/zmc_network/MainActivity.java
@@ -3,14 +3,50 @@ package com.example.zmc_network;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.os.StrictMode;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
+import java.io.IOException;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.Response;
 
 public class MainActivity extends AppCompatActivity {
-    
+    @BindView(R.id.get__information_button)
+    Button getInformationBtn;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        if (android.os.Build.VERSION.SDK_INT > 9) {
+            StrictMode.ThreadPolicy policy = new StrictMode.ThreadPolicy.Builder().permitAll().build();
+            StrictMode.setThreadPolicy(policy);
+        }
+        ButterKnife.bind(this);
+        getInformationBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                OkHttpClient client = new OkHttpClient();
+                Request request = new Request.Builder()
+                        .get()
+                        .url("https://twc-android-bootcamp.github.io/fake-data/data/default.json")
+                        .build();
+                
+
+                try (Response response = client.newCall(request).execute()) {
+                    Toast.makeText(MainActivity.this, response.body().string(), Toast.LENGTH_SHORT).show();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+        });
 
     }
 
