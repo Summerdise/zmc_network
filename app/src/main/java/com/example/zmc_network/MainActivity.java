6c5f22feae30a9ee1e4aa518baa14beff131f437
Summerdise <350464517@qq.com>
2020-08-19 16:12:15 +0800

    [zmc] fix: fix different time request status

--- app/src/main/java/com/example/zmc_network/MainActivity.java
+++ app/src/main/java/com/example/zmc_network/MainActivity.java
@@ -8,15 +8,21 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.Toast;
 
+import org.jetbrains.annotations.NotNull;
+
 import java.io.IOException;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
+import okhttp3.Call;
+import okhttp3.Callback;
 import okhttp3.OkHttpClient;
 import okhttp3.Request;
 import okhttp3.Response;
 
 public class MainActivity extends AppCompatActivity {
+    private final OkHttpClient client = new OkHttpClient();
+
     @BindView(R.id.get__information_button)
     Button getInformationBtn;
 
@@ -24,7 +30,6 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
         if (android.os.Build.VERSION.SDK_INT > 9) {
             StrictMode.ThreadPolicy policy = new StrictMode.ThreadPolicy.Builder().permitAll().build();
             StrictMode.setThreadPolicy(policy);
@@ -33,18 +38,32 @@ public class MainActivity extends AppCompatActivity {
         getInformationBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                OkHttpClient client = new OkHttpClient();
-                Request request = new Request.Builder()
+                final OkHttpClient client = new OkHttpClient();
+                final Request request = new Request.Builder()
                         .get()
                         .url("https://twc-android-bootcamp.github.io/fake-data/data/default.json")
                         .build();
-                
+                client.newCall(request).enqueue(new Callback() {
+                    @Override
+                    public void onResponse(@NotNull Call call, @NotNull Response response) throws IOException {
+                        runOnUiThread(new Runnable() {
+                            @Override
+                            public void run() {
+                                try (Response response = client.newCall(request).execute()) {
+                                    Toast.makeText(MainActivity.this, response.body().string(), Toast.LENGTH_SHORT).show();
+                                } catch (IOException e) {
+                                    e.printStackTrace();
+                                }
+                            }
+                        });
+                    }
 
-                try (Response response = client.newCall(request).execute()) {
-                    Toast.makeText(MainActivity.this, response.body().string(), Toast.LENGTH_SHORT).show();
-                } catch (IOException e) {
-                    e.printStackTrace();
-                }
+                    @Override
+                    public void onFailure(@NotNull Call call, @NotNull IOException e) {
+                        Toast.makeText(MainActivity.this, "no web connect", Toast.LENGTH_SHORT).show();
+                    }
+                });
+
             }
         });
 
