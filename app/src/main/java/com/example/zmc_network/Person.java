0e85d8ed24ddcd703b09167ca30f1aa5dc833b2f
Summerdise <350464517@qq.com>
2020-08-20 01:49:11 +0800

    [zmc] feat: add person and wrapper class

---
+++ app/src/main/java/com/example/zmc_network/Person.java
@@ -0,0 +1,6 @@
+package com.example.zmc_network;
+
+public class Person {
+    public String name;
+    public String avatar;
+}
---
+++ app/src/main/java/com/example/zmc_network/Wrapper.java
@@ -0,0 +1,11 @@
+package com.example.zmc_network;
+
+import java.util.ArrayList;
+
+public class Wrapper {
+    public ArrayList<Person> personList;
+
+    public Wrapper(ArrayList<Person> personList) {
+        this.personList = personList;
+    }
+}
